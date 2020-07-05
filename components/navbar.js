bf80435fa517899c0d9cc62958d882128e68e359
Patrick Kuang <pkuang@ucsb.edu>
2020-07-05 14:27:29 -0700

    pk - a little navbar styling

--- components/navbar.js
+++ components/navbar.js
@@ -3,16 +3,22 @@ import Link from 'next/link'
 export default function navbar() {
     let navbarStyle = {
         width: '100%',
-        backgroundColor: '#edf2f7',
+        backgroundColor: '#5ac0ca',
         height: '3rem',
         display: 'flex',
-        justifyContent: 'space-between'
-      }
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
 
-      return (
+    let tabStyle = {
+        color: '#fff', 
+        textDecoration: 'none'
+    }
+
+    return (
         <div style={navbarStyle}>
-          <Link href="/"><a>Coders SB</a></Link>
-          <Link href="/samplePage"><a>sample</a></Link>
+            <Link href="/"><a style={tabStyle}>Coders SB</a></Link>
+            <Link href="/samplePage"><a style={tabStyle}>sample</a></Link>
         </div>
-      );
-  }
+    );
+}
--- pages/samplePage.js
+++ pages/samplePage.js
@@ -1,7 +1,10 @@
+//sample page
+
 import Layout from '../components/layout'
 
 export default function samplePage() {
     return (
+        //must wrap your page in the Layout tag
         <Layout>
             <div>
                 <p>Sample Page</p>
