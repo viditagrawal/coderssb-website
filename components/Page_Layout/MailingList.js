050a18844cb6c8fdd8a4fdf4d9622f29f08d5f45
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-07-25 14:19:07 -0700

    mailing list functionality complete

--- components/Page_Layout/MailingList.js
+++ components/Page_Layout/MailingList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useMailChimpForm } from "use-mailchimp-form";
 
 const useFormFields = initialState => {
@@ -23,24 +23,46 @@ export default function MailingList() {
   const [params, handleFieldChange] = useFormFields({
     EMAIL: ""
   });
+
+  useEffect(() => {
+    var btn = document.getElementById("MailchimpSubmitBtn");
+    if (status.loading) {
+      btn.innerHTML = "Adding to Mailing List ...";
+      btn.disabled = true;
+    } else if (status.error) {
+      btn.innerHTML = "Err adding to Mailing List";
+    } else if (status.success) {
+      btn.innerHTML = "Added to Mailing List";
+      btn.disabled = true; //OR
+    }
+  }, [status]);
+
   return (
     <div className="w3-container w3-padding-64" id="contact">
       <h1>Join Our Mailing List</h1>
-      <br />
       <form onSubmit={event => handleSubmit(event, params)}>
+        <p>
+          In order to stay up to date with CodersSB's company events, technical
+          workshops, and networking events, make sure to join our mailing list!
+          You wouldn't want to miss out any important information!
+        </p>
         <input
           className="w3-input w3-padding-16"
           id="EMAIL"
           autoFocus
           type="email"
           value={params.EMAIL}
+          placeholder="your_email@gmail.com"
           onChange={handleFieldChange}
         />
-        <button>submit</button>
+        <button
+          id="MailchimpSubmitBtn"
+          className="w3-button w3-light-grey w3-section"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
-      {status.loading && message}
-      {status.error && message}
-      {status.success && message}
     </div>
   );
 }
