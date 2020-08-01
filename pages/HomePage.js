10953982a7ab45e16726cfb2202d7e0d6f8d2801
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-08-01 12:34:41 -0700

    addressed merge conflicts with Calender functionality

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,11 +2,8 @@ import Header from "../components/Page_Layout/Header";
 import About from "../components/Page_Layout/About";
 import Events from "../components/Page_Layout/Events";
 import Contact from "../components/Page_Layout/Contact";
-<<<<<<< HEAD
 import MailingList from "../components/Page_Layout/MailingList";
-=======
 import Calendar from "../components/Page_Layout/Calendar";
->>>>>>> a050b6b09cebd2c6bee9d8e0e5fdabfd12349165
 
 export default function HomePage() {
   return (
