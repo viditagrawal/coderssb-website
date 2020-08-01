c4f00e14edd5db683413e84aadde34d34c7e8d96
minibapat <mini.bapat@gmail.com>
2020-08-01 10:20:20 -0700

    Create Merchandise.js
    Next step: turn each product we're selling into a prop, create another js file with this file just being for adding new products in

---
+++ components/Page_Layout/Merchandise.js
@@ -0,0 +1,23 @@
+export default function Merchandise() {
+  return (
+    <div className="w3-row w3-padding-64" id="about">
+      <div className="w3-col m6 w3-padding-large w3-hide-small">
+        <img
+          src="shirt.png"
+          className="w3-round w3-image w3-opacity-min"
+          alt="Coders SB Tshirt"
+          width="600"
+          height="750"
+        />
+      </div>
+
+      <div className="w3-col m6 w3-padding-large">
+        <h1 className="w3-center">Coders SB T-Shirt</h1>
+        <h3 className="w3-center">Price: $15</h3>
+        <h3 className="w3-center">Available sizes: S M L</h3>
+        <h3 className="w3-center">Colors: Black, Blue, Gray</h3>
+        <br />
+      </div>
+    </div>
+  );
+}
