c419b4671f24e7a37dd95e437732954f2df22e61
minibapat <mini.bapat@gmail.com>
2020-08-27 19:41:57 -0700

    Update Merchandise.js

--- components/Page_Layout/Merchandise.js
+++ components/Page_Layout/Merchandise.js
@@ -1,16 +1,7 @@
 export default function Merchandise() {
   return (
     <div className="w3-row w3-padding-64" id="about">
-      <div className="w3-col m6 w3-padding-large w3-hide-small">
-        <img
-          src="shirt.png"
-          className="w3-round w3-image w3-opacity-min"
-          alt="Coders SB Tshirt"
-          width="600"
-          height="750"
-        />
-      </div>
-
+	  
       <div className="w3-col m6 w3-padding-large">
         <h1 className="w3-center">Coders SB T-Shirt</h1>
         <h3 className="w3-center">Price: $15</h3>
@@ -18,6 +9,16 @@ export default function Merchandise() {
         <h3 className="w3-center">Colors: Black, Blue, Gray</h3>
         <br />
       </div>
+	  
+      <div className="w3-col m6 w3-padding-large w3-hide-small">
+        <img
+          src="shirt.png"
+          className="w3-round w3-image w3-opacity-min"
+          alt="Coders SB Tshirt"
+          width="600"
+          height="600"
+        />
+      </div> 
      
     <div class = "Form">
 	<h2>Fill out the form below if you would like to order our merchandise!</h2>
