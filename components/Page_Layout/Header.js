e55ec7f31e4242a7472311b8da8c7caaeec85597
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-09-12 18:25:30 -0700

    implemented moving img slideshow

--- components/Page_Layout/Header.js
+++ components/Page_Layout/Header.js
@@ -1,20 +1,45 @@
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const images = [
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  },
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  },
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  },
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  },
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  },
+  {
+    original: "banner_2_header.png",
+    thumbnail: "banner_2.png"
+  }
+];
+
 export default function Header() {
   return (
-    <header
-      className="w3-display-container w3-content w3-wide"
-      style={{ maxWidth: 1600, minWidth: 500 }}
-      id="home"
-    >
-      <img
-        className="w3-image"
-        src="banner.png"
-        alt="Header"
-        width="1600"
-        height="800"
-      />
-      <div className="w3-display-bottomleft w3-padding-large w3-opacity">
-        <h1 className="w3-xxlarge">End of Header</h1>
-      </div>
-    </header>
+    <ImageGallery
+      autoPlay={true}
+      showPlayButton={false}
+      showNav={false}
+      slideInterval={4000}
+      slideDuration={1000}
+      thumbnailPosition={"bottom"}
+      slideOnThumbnailOver={true}
+      showFullscreenButton={false}
+      items={images}
+    />
   );
 }
