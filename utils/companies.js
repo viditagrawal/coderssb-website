77804db5d72a4b35055d52907b2368fa64771dab
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-09-15 22:36:15 -0700

    Added outline for array of company images for gallery=

---
+++ utils/companies.js
@@ -0,0 +1,29 @@
+const companyImages = [
+  {
+    src: "Companies/microsoft.png",
+    thumbnail: "Companies/microsoft.png",
+    thumbnailWidth: 700,
+    thumbnailHeight: 700,
+    caption: "Microsoft"
+  },
+  {
+    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+    thumbnail:
+      "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+
+    caption: "Boats (Jeshu John - designerspics.com)"
+  },
+  {
+    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    thumbnail:
+      "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: true,
+    caption: "After Rain (Jeshu John - designerspics.com)"
+  }
+];
+
+export default companyImages;
