0de0b3a197b7b5f52722c81553a1e7eb39c872a1
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-09-15 22:36:55 -0700

    displayed Companies component to main page

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -5,27 +5,49 @@ import Contact from "../components/Page_Layout/Contact";
 import MailingList from "../components/Page_Layout/MailingList";
 import Calendar from "../components/Page_Layout/Calendar";
 import Typing from "../components/Page_Layout/Typing";
+import BackgroundImage from "../components/Page_Layout/BackgroundImage";
+import Companies from "../components/Page_Layout/Companies";
 
 export default function HomePage() {
   return (
     <div>
       <Header />
-      {/* <-- Page content begins --> */}
+
       <div className="w3-content" style={{ maxWidth: 1100 }}>
-        <Typing />
         <hr />
         <Events />
         <hr />
-        <Calendar />
+      </div>
+
+      <Typing />
+
+      <div className="w3-content" style={{ maxWidth: 1100 }}>
         <hr />
         <About />
         <hr />
+      </div>
 
-        <Contact />
+      <Companies />
+      {/* <hr /> */}
+      <BackgroundImage
+        source="storke_background_blur.png"
+        missing_source="banner_1.png"
+        title={
+          <a href="https://www.facebook.com/groups/coderssb" target="_blank">
+            Click to Join our Facebook Page!
+          </a>
+        }
+        hover={true}
+      />
+
+      <div className="w3-content" style={{ maxWidth: 1100 }}>
         <hr />
         <MailingList />
+        <hr />
+        <Calendar />
+        <hr />
+        <Contact />
       </div>
-      {/* <-- End page content --> */}
     </div>
   );
 }
