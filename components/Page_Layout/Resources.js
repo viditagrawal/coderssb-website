bf7b4467448aa35def038059bf82fac344ced4f7
Pranav Acharya <pranavacharya@Pranavs-MacBook-Pro.local>
2020-09-27 18:50:56 -0700

    pa-resolved all of gm's comments. added resources for internships, coding interviews, and programming langs

--- components/Page_Layout/Resources.js
+++ components/Page_Layout/Resources.js
@@ -6,7 +6,7 @@ function NewResource(props) {
 
     const listOfLinks = []
     for (const [index, value] of props.resource.links.entries()) {
-        listOfLinks.push(<li key={index}><a href = {value}>{value}</a></li>)
+        listOfLinks.push(<li key={index}><a href = {value} target="_blank" style={{color:"#0000EE"}}>{value}</a></li>)  
       }
     
     return (
@@ -35,8 +35,8 @@ export default function Resources() {
     
     return (
         <div className="w3-container w3-padding-64">
-            <h1> Coding Resources</h1>
-            <p> Here are some resources that can help you learn a new programming skill. We also have tips on preparing for coding interviews, contacting professors, applying to internships, and more!</p>
+            <h1> Computer Science Resources</h1>
+            <p> Here are resources to help you contact professors for research, learn new programming skills, apply to internships, and more!</p>
             {resourceList}
         </div>
     )
--- utils/resourcelist.js
+++ utils/resourcelist.js
@@ -1,6 +1,27 @@
 
 export default [
     {
+        id: "4",
+        name: "Research at UCSB",
+        description: "Here is information about the various computer science research labs on campus and the faculty who lead them. Read about each of the labs and contact the faculty members who are in research areas that excite you.",
+        links: ["https://www.linkedin.com", "https://www.cs.ucsb.edu/people"],
+        image: "ucsbcs-logo.png",
+    },
+    {
+        id: "6",
+        name: "Applying to Internships",
+        description: "Remember to come to our awesome internship panel, resume workshop, and info sessions to learn how to prepare yourself to apply to internships. Also make sure to create accounts on LinkedIn and Handshake (through Career Services) and look for internships on these platforms. ",
+        links: ["https://www.cs.ucsb.edu/research", "https://career.ucsb.edu/handshake-help-center"],
+        image: "linkedin-icon.png",
+    },
+    {
+        id: "5",
+        name: "Preparing for Coding Interviews",
+        description: "Here are links to Leetcode and HackerRank, which have great practice problems to help you prepare for coding interviews for your internships.",
+        links: ["https://leetcode.com/problemset/all/", "https://www.hackerrank.com/"],
+        image: "leetcode-logo.png",
+    },
+    {
         id: "1",
         name: "React",
         description: "React is a front-end JavaScript framework. It's so popular because it makes front-end web development much simpler and easier to understand. In fact, this website was built using React! ",
@@ -11,8 +32,17 @@ export default [
         id: "2",
         name: "C++",
         description: "C++ is a powerful object-oriented language used to build complex applications, games, and more. The introductory CS courses here at UCSB teach C++.",
-        links: ["https://www.learncpp.com/", "https://www.udemy.com/course/free-learn-c-tutorial-beginners/"],
+        links: ["https://www.learncpp.com", "https://www.udemy.com/course/free-learn-c-tutorial-beginners"],
         image: "cpp_logo.png",
+
+        
+    },
+    {
+        id: "4",
+        name: "Android App Development",
+        description: "Learn how to create your own Android mobile apps using Kotlin, Java, or C++.",
+        links: ["https://tinyurl.com/coderssbandroidapp", "https://developer.android.com/training/basics/firstapp"],
+        image: "androidstudio-icon2.png",
 
         
     }
