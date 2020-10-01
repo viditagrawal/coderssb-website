638b75bca411c99f6588fd5ba6a705dece9e8cdf
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-01 13:30:27 -0700

    updated header images

--- utils/header.js
+++ utils/header.js
@@ -1,27 +1,35 @@
 const gallery_images = [
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/coderssb.png",
+    thumbnail: "Header/resized/coderssb_thumbnail.png"
   },
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/sunset_1.png",
+    thumbnail: "Header/resized/sunset_1.png"
   },
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/engineering.jpg",
+    thumbnail: "Header/resized/engineering_thumbnail.jpg"
   },
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/sunset_2.png",
+    thumbnail: "Header/resized/sunset_2_thumbnail.png"
   },
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/areial.jpg",
+    thumbnail: "Header/resized/areial.jpg"
   },
   {
-    original: "Header/coderssb.png",
-    thumbnail: "Header/coderssb_thumbnail.png"
+    original: "Header/resized/beach_1.jpg",
+    thumbnail: "Header/resized/beach_1.jpg"
+  },
+  {
+    original: "Header/resized/whiteBanner.png",
+    thumbnail: "Header/resized/whiteBanner.png"
+  },
+  {
+    original: "Header/resized/coderssb.png",
+    thumbnail: "Header/resized/coderssb_thumbnail.png"
   }
 ];
 
