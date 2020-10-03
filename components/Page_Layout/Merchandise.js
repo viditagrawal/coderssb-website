32a4678dfe52c20ba26424963b2195618a8ea355
minibapat <mini.bapat@gmail.com>
2020-10-02 18:43:15 -0700

    used future event card to reformat merch page

--- components/Page_Layout/Merchandise.js
+++ components/Page_Layout/Merchandise.js
@@ -1,42 +1,30 @@
+import FutureEventCard from "./FutureEventCard";
+import "../../css/FutureEventCardStyle.css";
+
 export default function Merchandise() {
   return (
     <div>
-	  
-      <div>
-        <h1>Coders SB T-Shirt</h1>
-        <h3>Price: $15</h3>
-        <h3>Available sizes: S M L</h3>
-        <h3>Colors: Black, Blue, Gray</h3>
-        <br />
-      </div>
-	  
-      <div>
-        <img
-          src="shirt.png"
-          className="w3-round w3-image w3-opacity-min"
-          alt="Coders SB Tshirt"
-          width="600"
-          height="600"
+      <h1>Buy CodersSB Merch!</h1>
+      <div className="all">
+        <FutureEventCard
+          imgUrl={"shirt.png"}
+          when={"Coders SB T-shirt"}
+          info={"Price: $15 | Sizes: S M L"}
         />
-      </div> 
-	  
-      <div>
-        <img
-          src="shirt.png"
-          className="w3-round w3-image w3-opacity-min"
-          alt="Coders SB Tshirt"
-          width="600"
-          height="600"
+
+        <FutureEventCard
+          imgUrl={"shirtgroup.png"}
+          when={"Here's what our shirts look like!"}
         />
-      </div> 
-     
-      <div>
-	<h2>Fill out the form below if you would like to order our merchandise!</h2>
-        <h3><a href="https://forms.gle/CoVxX6HQeCbaEyqVA" target="_blank">Order Form</a></h3>
+
+        <FutureEventCard
+          imgUrl={"sticker.png"}
+          when={"Coders SB Laptop Sticker"}
+          info={"Price $2"}
+        />
       </div>
-    
+      <h2>Fill out the form below if you would like to order our merchandise!</h2>
+      <h3><a href="https://forms.gle/CoVxX6HQeCbaEyqVA" target="_blank">Order Form</a></h3>
     </div>
-    
-    
   );
 }
