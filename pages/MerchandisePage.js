a01c7d29544c46a7b7bba981d7cef60f9d28ce0a
minibapat <mini.bapat@gmail.com>
2020-10-03 16:03:36 -0700

    fixing styling to look like home page

--- pages/MerchandisePage.js
+++ pages/MerchandisePage.js
@@ -1,10 +1,12 @@
 //merchandise page for selling tshirt and stickers
 import Layout from "../components/Website_Layout/Layout";
 import Merchandise from "../components/Page_Layout/Merchandise";
+import Header from "../components/Page_Layout/Header";
 
 export default function MerchandisePage() {
   return (
     <div>
+      <Header>
       {/* <-- Page content begins --> */}
       <div className="w3-content" style={{ maxWidth: 1100 }}>
         <Merchandise />
