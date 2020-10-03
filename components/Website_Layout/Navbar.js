5e63b5f3f2fb2bc7dd6fde98e4c954389f71e0dc
Patrick Kuang <pkuang@ucsb.edu>
2020-10-03 13:50:12 -0700

    pk - finish navbar

--- components/Website_Layout/Navbar.js
+++ components/Website_Layout/Navbar.js
@@ -1,26 +1,13 @@
 import Link from "next/link";
 import { GoogleLogin } from "react-google-login";
 import React, { useState } from 'react'
+import '../../css/navbar.css'
 
 export default function navbar(props) {
 
   const [loggedIn, setLoggedIn] = useState(false)
   const [username, setUsername] = useState("")
   const [profileImgUrl, setProfileImgUrl] = useState("")
-  
-  let navbarStyle = {
-    width: "100%",
-    backgroundColor: "#5ac0ca",
-    height: "3rem",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center"
-  };
-
-  let tabStyle = {
-    color: "#fff",
-    textDecoration: "none"
-  };
 
   const responseGoogle = (googleUser) => {
     var googleId = googleUser.getId();
@@ -33,28 +20,37 @@ export default function navbar(props) {
   }
 
   return (
-    <div style={navbarStyle}>
+    <div className="navbar">
       <Link href="/">
-        <a style={tabStyle}>Coders SB</a>
+        <img 
+          style={{cursor: 'pointer'}}
+          className="w3-image"
+          src="logo.png"
+          alt="Header"
+          width="100"
+          height="50"
+        />
       </Link>
-      <Link href="/samplePage">
-        <a style={tabStyle}>sample</a>
-      </Link>
-      <Link href="/MerchandisePage">
-        <a style={tabStyle}>Buy our Merchandise!</a>
-      </Link>
-      {loggedIn ? <img style={{borderRadius: '9999px', height: '3rem', width: '3rem' }} src={profileImgUrl}/>:
-      <GoogleLogin
-        render={renderProps => (
-          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
-        )}
-        clientId= {process.env.LOCAL_GOOGLE_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />}
+      <div>
+        <Link href="/samplePage">
+          <a className="tab">sample</a>
+        </Link>
+        <Link href="/MerchandisePage">
+          <a className="tab">Buy our Merchandise!</a>
+        </Link>
+        {loggedIn ? <img style={{ borderRadius: '9999px', height: '3rem', width: '3rem' }} src={profileImgUrl} /> :
+          <GoogleLogin
+            render={renderProps => (
+              <button className="login" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
+            )}
+            clientId={process.env.GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+          />}
+      </div>
     </div>
-    
+
   );
 }
