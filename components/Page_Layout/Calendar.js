1d154c061b284db5d08a16e319156a1da5e1a05d
sriya112 <sriyaaluru@gmail.com>
2020-10-04 00:25:34 -0400

    prev changes

--- components/Page_Layout/Calendar.js
+++ components/Page_Layout/Calendar.js
@@ -9,32 +9,28 @@ import events from "../../utils/events";
 import "../../css/CalendarStyle.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-
 export default function Calendar() {
   const localizer = momentLocalizer(moment);
 
   // Views are the Day/Week/Month/Work Week/Agenda views. Right now the default has all views.
   let allViews = Object.keys(Views).map((k) => Views[k]);
 
-let allViews = Object.keys(Views).map(k => Views[k])
-
-return (
-  <div>
-    <div style={{ height: '500pt'}}>
-      <BigCalendar
-        events={events}
-        views={allViews}
-        step={60}
-        popup = {false}
-        startAccessor="start"
-        endAccessor="end"
-        showMultiDayTimes
-        defaultDate={moment().toDate()}
-        localizer={localizer}
-        components={{event: CustomEvent,}}
-  />
+  return (
+    <div>
+      <div style={{ height: "500pt" }}>
+        <BigCalendar
+          events={events}
+          views={allViews}
+          step={60}
+          popup={false}
+          startAccessor="start"
+          endAccessor="end"
+          showMultiDayTimes
+          defaultDate={moment().toDate()}
+          localizer={localizer}
+          components={{ event: CustomEvent }}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
 }
-
