4bcda9e4883fd7a75d3333b7b0576de7cca80645
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-04 02:42:33 -0700

    added About page and removed samplePage from nav

--- components/Website_Layout/Navbar.js
+++ components/Website_Layout/Navbar.js
@@ -1,18 +1,13 @@
 import Link from "next/link";
 import { GoogleLogin } from "react-google-login";
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
-import React, { useEffect, useState } from 'react'
-import '../../css/navbar.css'
->>>>>>> 6bcd8a8b3671155efd1f143e97f508d93d2d841a
+import React, { useEffect, useState } from "react";
+import "../../css/navbar.css";
 
 export default function navbar(props) {
   const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [profileImgUrl, setProfileImgUrl] = useState("");
 
-<<<<<<< HEAD
   let navbarStyle = {
     width: "100%",
     backgroundColor: "#5ac0ca",
@@ -21,28 +16,23 @@ export default function navbar(props) {
     justifyContent: "space-between",
     alignItems: "center"
   };
-=======
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [profileImgUrl, setProfileImgUrl] = useState("")
->>>>>>> 6bcd8a8b3671155efd1f143e97f508d93d2d841a
 
   useEffect(() => {
-    if (props.googleUserObject)
-    {
-      setLoggedIn(true)
-      setProfileImgUrl(props.googleUserObject.profileObj.imageUrl)
+    if (props.googleUserObject) {
+      setLoggedIn(true);
+      setProfileImgUrl(props.googleUserObject.profileObj.imageUrl);
     }
-  }, [props.googleUserObject])
+  }, [props.googleUserObject]);
 
-  const responseGoogle = (googleUser) => {
-    props.setGoogleUserObject(googleUser)
-  }
+  const responseGoogle = googleUser => {
+    props.setGoogleUserObject(googleUser);
+  };
 
   return (
     <div className="navbar">
       <Link href="/">
-        <img 
-          style={{cursor: 'pointer'}}
+        <img
+          style={{ cursor: "pointer" }}
           className="w3-image"
           src="logo.png"
           alt="Header"
@@ -51,30 +41,39 @@ export default function navbar(props) {
         />
       </Link>
       <div>
-        <Link href="/samplePage">
-          <a className="tab">sample</a>
+        <Link href="/About">
+          <a className="tab">About Us</a>
         </Link>
         <Link href="/MerchandisePage">
           <a className="tab">Buy our Merchandise!</a>
         </Link>
         <Link href="/CalendarPage">
-        <a className="tab">Calendar</a>
-      </Link>
-        {loggedIn ? 
-          <img style={{ borderRadius: '9999px', height: '3rem', width: '3rem' }} src={profileImgUrl} />
-          :
+          <a className="tab">Calendar</a>
+        </Link>
+        {loggedIn ? (
+          <img
+            style={{ borderRadius: "9999px", height: "3rem", width: "3rem" }}
+            src={profileImgUrl}
+          />
+        ) : (
           <GoogleLogin
             render={renderProps => (
-              <button className="login" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
+              <button
+                className="login"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Login
+              </button>
             )}
             clientId={process.env.GOOGLE_CLIENT_ID}
             buttonText="Login"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={"single_host_origin"}
-          />}
+          />
+        )}
       </div>
     </div>
-
   );
 }
