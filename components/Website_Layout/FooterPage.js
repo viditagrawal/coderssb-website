4fc6a5efc9eb04e8570d174a283290a88b9b8720
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-04 02:40:09 -0700

    added fully complete footer to all pages

--- components/Website_Layout/FooterPage.js
+++ components/Website_Layout/FooterPage.js
@@ -1,23 +1,78 @@
 import Footer from "rc-footer";
 import "rc-footer/assets/index.css"; // import 'rc-footer/asssets/index.less';
-// import { render } from "react-dom";
 
 export default function FooterPage() {
   return (
     <Footer
-      backgroundColor="rgb(250, 176, 60)"
+      maxColumnsPerRow={4}
+      backgroundColor="rgb(34, 68, 122)"
       columns={[
         {
-          icon: (
-            <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
-          ),
-          title: "Footer Title",
-          url: "google.com",
-          description: "nice",
-          openExternal: true
+          title: "Learn More",
+          items: [
+            {
+              icon: <img src="Icons/facebook.png" />,
+              title: "Facebook",
+              url: "https://www.facebook.com/coderssb",
+              openExternal: true
+            },
+            {
+              icon: <img src="Icons/instagram.png" />,
+              title: "Instagram",
+              url: "https://www.instagram.com/coders.sb/",
+              openExternal: true
+            },
+            {
+              icon: <img src="Icons/ucsb.png" />,
+              title: "Shoreline",
+              url: "https://shoreline.ucsb.edu/student_community?club_id=29769",
+              openExternal: true
+            }
+          ]
+        },
+        {
+          title: "Contact Us",
+          items: [
+            {
+              icon: <img src="Icons/email.png" />,
+              title: "Email",
+              url:
+                "https://mail.google.com/mail/?view=cm&source=mailto&to=coderssb@gmail.com",
+              openExternal: true
+            },
+            {
+              icon: <img src="Icons/mailchimp.png" />,
+              title: "Mailing List",
+              url: "http://eepurl.com/gPudI1",
+              openExternal: true
+            }
+          ]
+        },
+        {
+          title: "Other CS UCSB Orgs",
+          items: [
+            {
+              icon: <img src="Icons/swe.png" />,
+              title: "UCSB SWE",
+              url: "https://ucsbswe.weebly.com",
+              openExternal: true
+            },
+            {
+              icon: <img src="Icons/thetatau.png" />,
+              title: "Theta Tau",
+              url: "https://sbthetatau.herokuapp.com",
+              openExternal: true
+            },
+            {
+              icon: <img src="Icons/sbhacks.png" />,
+              title: "SB Hacks",
+              url: "https://sbhacks.com",
+              openExternal: true
+            }
+          ]
         }
       ]}
-      bottom="Coders SB"
+      bottom="Coders SB ©"
     />
   );
 }
