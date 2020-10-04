528698a3ab58534cb0ade8dde646b9844f4db70f
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-04 02:44:00 -0700

    moved some information to diff pages

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,17 +1,10 @@
 import Header from "../components/Page_Layout/Header";
 import AboutUs from "../components/Page_Layout/AboutUs";
 import Events from "../components/Page_Layout/Events";
-<<<<<<< HEAD
-import Calendar from "../components/Page_Layout/Calendar";
 import Typing from "../components/Page_Layout/Typing";
 import BackgroundImage from "../components/Page_Layout/BackgroundImage";
 import Companies from "../components/Page_Layout/Companies";
 import Resources from "../components/Page_Layout/Resources";
-=======
-import Contact from "../components/Page_Layout/Contact";
-import Resources from "../components/Page_Layout/Resources";
-import MailingList from "../components/Page_Layout/MailingList";
->>>>>>> 6bcd8a8b3671155efd1f143e97f508d93d2d841a
 import FutureEventCard from "../components/Page_Layout/FutureEventCard";
 
 export default function HomePage() {
@@ -23,17 +16,6 @@ export default function HomePage() {
         <Events />
         <hr />
       </div>
-
-      <Typing />
-
-      <div className="w3-content" style={{ maxWidth: 1100 }}>
-        <hr />
-        <AboutUs />
-      </div>
-
-      <hr />
-      <Companies />
-      <hr />
       <BackgroundImage
         source="Background/rfilm/4_wide_blur.png"
         missing_source="banner_1.png"
@@ -43,7 +25,7 @@ export default function HomePage() {
             href="https://docs.google.com/forms/d/e/1FAIpQLSdP77TCVm-nBlPnLEXjojm-bp6NbfKak20_dz1LHkCyx-D7bw/viewform"
             target="_blank"
           >
-            Apply to be a Coders SB Officer
+            Click to Apply to be a Coders SB Officer
           </a>
         }
         hover={true}
@@ -55,10 +37,10 @@ export default function HomePage() {
         title={
           <a
             className={"blueHighlight"}
-            href="https://www.facebook.com/groups/coderssb"
+            href="https://www.facebook.com/coderssb"
             target="_blank"
           >
-            Click to Join our Facebook Page!
+            Click to Follow our Facebook Page
           </a>
         }
         hover={true}
