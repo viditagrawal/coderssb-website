68f75fc1955f51702d1f3caa6b586aac7f3bf54d
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-04 14:51:05 -0700

    made links inline

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,8 +1,8 @@
 import Header from "../components/Page_Layout/Header";
 import Events from "../components/Page_Layout/Events";
-import BackgroundImage from "../components/Page_Layout/BackgroundImage";
-import Resources from "../components/Page_Layout/Resources";
+import Links from "../components/Page_Layout/Links";
 import FutureEventCard from "../components/Page_Layout/FutureEventCard";
+import "../css/Links.css";
 
 export default function HomePage() {
   return (
@@ -13,50 +13,58 @@ export default function HomePage() {
         <Events />
         <hr />
       </div>
-      <BackgroundImage
-        source="Background/rfilm/8_wide_blur.png"
-        missing_source="banner_1.png"
-        title={
-          <a
-            className={"yellowHighlight"}
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdP77TCVm-nBlPnLEXjojm-bp6NbfKak20_dz1LHkCyx-D7bw/viewform"
-            target="_blank"
-          >
-            Click to Apply to be a Coders SB Officer
-          </a>
-        }
-        hover={true}
-      />
-      <hr />
-      <BackgroundImage
-        source="Background/rfilm/1_wide_blur.png"
-        missing_source="banner_1.png"
-        title={
-          <a
-            className={"blueHighlight"}
-            href="https://www.facebook.com/coderssb"
-            target="_blank"
-          >
-            Click to Follow our Facebook Page
-          </a>
-        }
-        hover={true}
-      />
-      <hr />
-      <BackgroundImage
-        source="Background/henley_background_blur.png"
-        missing_source="banner_1.png"
-        title={
-          <a
-            className={"yellowHighlight"}
-            href="http://eepurl.com/gPudI1"
-            target="_blank"
-          >
-            Click to Join Our Mailing List
-          </a>
-        }
-        hover={true}
-      />
+
+      <div style={{ width: "100%" }}>
+        <div style={{ float: "left", width: "33%" }}>
+          <Links
+            source="Background/rfilm/8_blur.png"
+            missing_source="banner_1.png"
+            title={
+              <a
+                className={"yellowHighlight"}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdP77TCVm-nBlPnLEXjojm-bp6NbfKak20_dz1LHkCyx-D7bw/viewform"
+                target="_blank"
+              >
+                Apply to be a Coders SB Officer
+              </a>
+            }
+            hover={true}
+          />
+        </div>
+        <div style={{ float: "left", width: "33%" }}>
+          <Links
+            source="Background/rfilm/1_blur.png"
+            missing_source="banner_1.png"
+            title={
+              <a
+                className={"blueHighlight"}
+                href="https://www.facebook.com/coderssb"
+                target="_blank"
+              >
+                Follow our Facebook Page
+              </a>
+            }
+            hover={true}
+          />
+        </div>
+        <div style={{ float: "left", width: "33%" }}>
+          <Links
+            source="Background/rfilm/6_blur.png"
+            missing_source="banner_1.png"
+            title={
+              <a
+                className={"yellowHighlight"}
+                href="http://eepurl.com/gPudI1"
+                target="_blank"
+              >
+                Join Our Mailing List
+              </a>
+            }
+            hover={true}
+          />
+        </div>
+        <br style={{ clear: "left" }} />
+      </div>
       <hr />
     </div>
   );
