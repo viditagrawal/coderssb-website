c1f73c27b8ce998802a8edd2732155f7300163a1
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-10-04 22:30:02 -0700

    t

--- components/Website_Layout/Layout.js
+++ components/Website_Layout/Layout.js
@@ -30,6 +30,17 @@ function Layout(props) {
           rel="stylesheet"
           href="https://www.w3schools.com/w3css/4/w3.css"
         />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
       </Head>
       <Navbar
         googleUserObject={googleUserObject}
