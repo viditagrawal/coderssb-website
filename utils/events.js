353e6f02681e6f37320f8ce887f48bfcc37d72c0
sriya112 <sriyaaluru@gmail.com>
2020-10-06 18:30:10 -0400

    fixed dates and linked events from fb page

--- utils/events.js
+++ utils/events.js
@@ -7,40 +7,39 @@ export const events = [
   {
     id: 1,
     title: "Test Event 1",
-    start: new Date(2020, 8, 1, 19, 30, 0), // 8 = SEPTEMBER, months start from 0 (January)
-    end: new Date(2020, 8, 1, 20, 30, 0),
-    url: "https://www.facebook.com/groups/coderssb/", //I have a placeholder here for now, until we have actual fb event pages
+    start: new Date(2020, 9, 13, 19, 30, 0), // 8 = SEPTEMBER, months start from 0 (January)
+    end: new Date(2020, 9, 13, 20, 30, 0),
+    url: "https://www.facebook.com/events/336856614058713/", //I have a placeholder here for now, until we have actual fb event pages
     info: "",
-    pic: "resources_images/event1.jpg",
+    pic: "resources_images/Oct132020.jpg",
   },
   {
     id: 2,
     title: "Test Event 2",
-    start: new Date(2020, 7, 10, 18, 0, 0), // 7 = AUGUST, months start from 0 (January)
-    end: new Date(2020, 7, 10, 20, 0, 0),
+    start: new Date(2020, 9, 15, 18, 0, 0), // 7 = AUGUST, months start from 0 (January)
+    end: new Date(2020, 9, 15, 20, 0, 0),
 
-    url: "https://www.facebook.com/groups/coderssb/", //placeholder
+    url: "https://www.facebook.com/events/796825201106990/", //placeholder
     info: "",
-    pic: "resources_images/event2.jpg",
+    pic: "resources_images/Oct152020.jpg",
   },
   {
     id: 3,
     title: "Test Event 3",
-    start: new Date(2020, 10, 10, 18, 0, 0), // 11 = NOVEMBER, months start from 0 (January)
-    end: new Date(2020, 7, 10, 20, 0, 0),
-    url: "https://www.facebook.com/groups/coderssb/", //placeholder
+    start: new Date(2020, 9, 20, 18, 0, 0), // 11 = NOVEMBER, months start from 0 (January)
+    end: new Date(2020, 9, 20, 20, 0, 0),
+    url: "https://www.facebook.com/events/1213889215657466/", //placeholder
     info: "",
-    pic: "resources_images/event3.png",
+    pic: "resources_images/Oct202020.jpg",
   },
   {
     id: 4,
     title: "Test Event 4",
-    start: new Date(2020, 9, 1, 18, 0, 0), // 9 = OCTOBER, months start from 0 (January)
-    end: new Date(2020, 9, 1, 18, 20, 0),
-    url: "http://facebook.com", //placeholder
-    info: "fourth event info",
-    pic:
-      "https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/15196011_1167971753282681_4498467004737646061_o.jpg?_nc_cat=110&_nc_sid=ca434c&_nc_ohc=yDkoSm8Do-8AX_P3UZr&_nc_ht=scontent-atl3-1.xx&oh=04b38411617f53767cf986084f289f84&oe=5F95C05A",
+    start: new Date(2020, 9, 28, 18, 0, 0), // 9 = OCTOBER, months start from 0 (January)
+    end: new Date(2020, 9, 28, 18, 20, 0),
+    url: "", //placeholder
+    info: "",
+    pic: "resources_images/Oct282020.png",
   },
   {
     id: 5,
