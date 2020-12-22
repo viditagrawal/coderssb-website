ca7f558b535217b34375526a625963cf474f50dc
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-12-21 22:40:01 -0800

    gm - removed extra header images

--- utils/resourcelist.js
+++ utils/resourcelist.js
@@ -1,56 +1,77 @@
-
 export default [
-    {
-        id: "4",
-        name: "Research and Courses at UCSB",
-        description: "Here is information about the various computer science research labs on campus and the faculty who lead them. Read about each of the labs and contact the faculty members who are in research areas that excite you. We also have a link to the CS courses offered by the department.",
-        links: ["https://www.cs.ucsb.edu/research", "https://www.cs.ucsb.edu/people","https://www.cs.ucsb.edu/education/courses/descriptions"],
-        image: "resources_images/ucsbcs-logo.png",
-    },
-    {
-        id: "6",
-        name: "Applying to Internships",
-        description: "Remember to come to our awesome internship panel, resume workshop, and info sessions to learn how to best prepare yourself for internship applications. Also, make sure to create accounts on LinkedIn and Handshake (through UCSB Career Services) and look for internships on these platforms. ",
-        links: ["https://www.linkedin.com", "https://career.ucsb.edu/handshake-help-center"],
-        image: "resources_images/linkedin-icon.png",
-    },
-    {
-        id: "5",
-        name: "Preparing for Coding Interviews",
-        description: "Here are links to Leetcode and HackerRank, which have great practice problems to help you prepare for coding interviews for your internships.",
-        links: ["https://leetcode.com/problemset/all/", "https://www.hackerrank.com/"],
-        image: "resources_images/leetcode-logo.png",
-    },
-    {
-        id: "7",
-        name: "SB Hacks",
-        description: "Make sure to check out SB Hacks, UCSB's very own hackathon that takes place in January. Visit the link below to learn more and sign up for their mailing list for updates.",
-        links: ["https://sbhacks.com/"],
-        image: "resources_images/sbhacks-logo.png",
-    },
-    {
-        id: "1",
-        name: "React",
-        description: "React is a front-end JavaScript framework. It's so popular because it makes front-end web development much simpler and easier to understand. In fact, this website was built using React! ",
-        links: ["https://scrimba.com/g/glearnreact", "https://reactjs.org/tutorial/tutorial.html"],
-        image: "resources_images/reactlogo.png",
-    },
-    {
-        id: "2",
-        name: "C++",
-        description: "C++ is a powerful object-oriented language used to build complex applications, games, and more. The introductory CS courses here at UCSB teach C++.",
-        links: ["https://www.learncpp.com", "https://www.udemy.com/course/free-learn-c-tutorial-beginners"],
-        image: "resources_images/cpp_logo.png",
-
-        
-    },
-    {
-        id: "3",
-        name: "Android App Development",
-        description: "Learn how to create your own Android mobile apps using Kotlin, Java, or C++.",
-        links: ["https://tinyurl.com/coderssbandroidapp", "https://developer.android.com/training/basics/firstapp"],
-        image: "resources_images/androidstudio-icon.png",
-
-        
-    }
-]
+  {
+    id: "4",
+    name: "Research and Courses at UCSB",
+    description:
+      "Here is information about the various computer science research labs on campus and the faculty who lead them. Read about each of the labs and contact the faculty members who are in research areas that excite you. We also have a link to the CS courses offered by the department.",
+    links: [
+      "https://www.cs.ucsb.edu/research",
+      "https://www.cs.ucsb.edu/people",
+      "https://www.cs.ucsb.edu/education/courses/descriptions"
+    ],
+    image: "resources_images/ucsbcs-logo.png"
+  },
+  {
+    id: "6",
+    name: "Applying to Internships",
+    description:
+      "Remember to come to our awesome internship panel, resume workshop, and info sessions to learn how to best prepare yourself for internship applications. Also, make sure to create accounts on LinkedIn and Handshake (through UCSB Career Services) and look for internships on these platforms. ",
+    links: [
+      "https://www.linkedin.com",
+      "https://career.ucsb.edu/handshake-help-center"
+    ],
+    image: "resources_images/linkedin-icon.png"
+  },
+  {
+    id: "5",
+    name: "Preparing for Coding Interviews",
+    description:
+      "Here are links to Leetcode and HackerRank, which have great practice problems to help you prepare for coding interviews for your internships.",
+    links: [
+      "https://leetcode.com/problemset/all/",
+      "https://www.hackerrank.com/"
+    ],
+    image: "resources_images/leetcode-logo.png"
+  },
+  {
+    id: "7",
+    name: "SB Hacks",
+    description:
+      "Make sure to check out SB Hacks, UCSB's very own hackathon that takes place in January. Visit the link below to learn more and sign up for their mailing list for updates.",
+    links: ["https://sbhacks.com/"],
+    image: "resources_images/sbhacks-logo.png"
+  },
+  {
+    id: "1",
+    name: "React",
+    description:
+      "React is a front-end JavaScript framework. It's so popular because it makes front-end web development much simpler and easier to understand. In fact, this website was built using React! ",
+    links: [
+      "https://scrimba.com/g/glearnreact",
+      "https://reactjs.org/tutorial/tutorial.html"
+    ],
+    image: "resources_images/reactlogo.png"
+  },
+  {
+    id: "2",
+    name: "C++",
+    description:
+      "C++ is a powerful object-oriented language used to build complex applications, games, and more. The introductory CS courses here at UCSB teach C++.",
+    links: [
+      "https://www.learncpp.com",
+      "https://www.udemy.com/course/free-learn-c-tutorial-beginners"
+    ],
+    image: "resources_images/cpp_logo.png"
+  },
+  {
+    id: "3",
+    name: "Android App Development",
+    description:
+      "Learn how to create your own Android mobile apps using Kotlin, Java, or C++.",
+    links: [
+      "https://tinyurl.com/coderssbandroidapp",
+      "https://developer.android.com/training/basics/firstapp"
+    ],
+    image: "resources_images/androidstudio-icon.png"
+  }
+];
