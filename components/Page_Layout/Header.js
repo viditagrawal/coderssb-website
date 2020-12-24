633b607d2bc04ff78d214cb8d4385a425655f2fc
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-12-23 17:35:39 -0800

    gm - update header imgs

--- components/Page_Layout/Header.js
+++ components/Page_Layout/Header.js
@@ -1,6 +1,7 @@
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import gallery_images from "../../utils/header";
+import "../../css/Header.css";
 
 const typing_wait_ms = 4000;
 
--- utils/header.js
+++ utils/header.js
@@ -1,39 +1,27 @@
 const gallery_images = [
   {
-    original: "Header/resized/coderssb.png",
-    thumbnail: "Header/resized/coderssb_thumbnail.png"
+    original: "Header/resized/w21_project_series.png",
+    thumbnail: "Header/resized/w21_project_series.png"
   },
   {
-    original: "Header/resized/sunset_1.png",
-    thumbnail: "Header/resized/sunset_1.png"
+    original: "Header/resized/lib.png",
+    thumbnail: "Header/resized/lib.png"
   },
   {
-    original: "Header/resized/engineering.jpg",
-    thumbnail: "Header/resized/engineering_thumbnail.jpg"
+    original: "Header/resized/sunset.png",
+    thumbnail: "Header/resized/sunset.png"
   },
   {
-    original: "Header/resized/whiteBanner.png",
-    thumbnail: "Header/resized/whiteBanner.png"
+    original: "Header/resized/light_lagoon.png",
+    thumbnail: "Header/resized/light_lagoon.png"
   },
   {
-    original: "Header/resized/sunset_2.png",
-    thumbnail: "Header/resized/sunset_2_thumbnail.png"
+    original: "Header/resized/storke.png",
+    thumbnail: "Header/resized/storke.png"
   },
   {
-    original: "Header/resized/areial.jpg",
-    thumbnail: "Header/resized/areial.jpg"
-  },
-  {
-    original: "Header/resized/beach_1.jpg",
-    thumbnail: "Header/resized/beach_1.jpg"
-  },
-  {
-    original: "Header/resized/whiteBanner.png",
-    thumbnail: "Header/resized/whiteBanner.png"
-  },
-  {
-    original: "Header/resized/coderssb.png",
-    thumbnail: "Header/resized/coderssb_thumbnail.png"
+    original: "Header/resized/dark_lagoon.png",
+    thumbnail: "Header/resized/dark_lagoon.png"
   }
 ];
 
