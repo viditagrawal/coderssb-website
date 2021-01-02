ed34c0bf1f9f0fbb856c00bab0588d9707c48fce
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-01-02 01:08:21 -0800

    gm - commented out futureEventCardStyle and replaced with npm module

--- components/Page_Layout/Events.js
+++ components/Page_Layout/Events.js
@@ -1,6 +1,6 @@
 import FutureEventCard from "./FutureEventCard";
 import { events } from "../../utils/events";
-import "../../css/FutureEventCardStyle.css";
+import "../../css/Hover-reveal-top-right.css";
 
 function DateConv(dateObj) {
   return (
@@ -20,29 +20,51 @@ export default function Events() {
   return (
     <div>
       <div className="all">
-        <FutureEventCard className="paragraph-text"
-          website={events[2].url}
-          imgUrl={events[2].pic}
-          altImgUrl={events[2].alt}
-          when={DateConv(events[2].start)}
-          info={events[2].info}
+        <figure>
+          <img src={events[2].pic} />
+          <figcaption>
+            {events[2].info + " " + DateConv(events[2].start)}
+          </figcaption>
+        </figure>
+        <figure>
+          <img src={events[3].pic} />
+          <figcaption>
+            {events[3].info + " " + DateConv(events[3].start)}
+          </figcaption>
+        </figure>
+        <figure>
+          <img src={events[5].pic} />
+          <figcaption>
+            {events[5].info + " " + DateConv(events[5].start)}
+          </figcaption>
+        </figure>
+
+        {/* <FutureEventCard
+          className="paragraph-text"
+          website={events[0].url}
+          imgUrl={events[0].pic}
+          altImgUrl={events[0].alt}
+          when={DateConv(events[0].start)}
+          info={events[0].info}
         />
 
-        <FutureEventCard className="paragraph-text"
-          website={events[4].url}
-          imgUrl={events[4].pic}
-          altImgUrl={events[4].alt}
-          when={DateConv(events[4].start)}
-          info={events[4].info}
+        <FutureEventCard
+          className="paragraph-text"
+          website={events[1].url}
+          imgUrl={events[1].pic}
+          altImgUrl={events[1].alt}
+          when={DateConv(events[1].start)}
+          info={events[1].info}
         />
 
-        <FutureEventCard className="paragraph-text"
+        <FutureEventCard
+          className="paragraph-text"
           website={events[5].url}
           imgUrl={events[5].pic}
           altImgUrl={events[5].alt}
           when={DateConv(events[5].start)}
           info={events[5].info}
-        />
+        /> */}
       </div>
     </div>
   );
