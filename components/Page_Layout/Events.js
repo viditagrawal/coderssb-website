73596156695804de9240de6dc285a4e1c58c8a69
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-01-04 14:45:37 -0800

    gm - cleaned up hover text for events

--- components/Page_Layout/Events.js
+++ components/Page_Layout/Events.js
@@ -24,18 +24,18 @@ export default function Events() {
         <EventCard
           url={events[2].url}
           pic={events[2].pic}
-          info={DateConv(events[2].start) + " | Click to learn more!"}
+          info={DateConv(events[2].start) + " Click to learn more!"}
         />
 
         <EventCard
           url={events[3].url}
           pic={events[3].pic}
-          info={DateConv(events[3].start) + " | Click to learn more!"}
+          info={DateConv(events[3].start) + " Click to learn more!"}
         />
         <EventCard
           url={events[5].url}
           pic={events[5].pic}
-          info={DateConv(events[5].start) + " | Click to learn more!"}
+          info={DateConv(events[5].start) + " Click to learn more!"}
         />
       </div>
     </div>
