66ce8abaebf0702e2b22f1b8034e73683cd09207
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-01-21 21:07:15 -0800

    gm - removed prof coffee talk

--- utils/events.js
+++ utils/events.js
@@ -42,7 +42,7 @@ export const events = [
     alt: "Introduction to react project series"
   },
   {
-    id: 4,
+    id: 3,
     title: "Project Series: React fundamentals",
     start: new Date(2021, 0, 21, 19, 0, 0),
     end: new Date(2021, 0, 21, 20, 30, 0),
@@ -52,7 +52,7 @@ export const events = [
     alt: "Learning the fundamentals of react project series graphic"
   },
   {
-    id: 5,
+    id: 4,
     title: "Landing Your First Interview",
     start: new Date(2021, 0, 19, 18, 30, 0),
     end: new Date(2021, 0, 19, 19, 30, 0),
@@ -63,7 +63,7 @@ export const events = [
     alt: "Landing your first interview image"
   },
   {
-    id: 6,
+    id: 5,
     title: "Project Series: Google OAuth Sign In",
     start: new Date(2021, 0, 28, 19, 0, 0),
     end: new Date(2021, 0, 28, 20, 30, 0),
@@ -74,7 +74,7 @@ export const events = [
     alt: "Google OAuth sign in tutorial"
   },
   {
-    id: 7,
+    id: 6,
     title: "Project Series: Midpoint Showcase",
     start: new Date(2021, 1, 4, 19, 0, 0),
     end: new Date(2021, 1, 4, 20, 30, 0),
@@ -85,16 +85,6 @@ export const events = [
   },
   {
     id: 8,
-    title: "CS Professor Coffee Talk",
-    start: new Date(2021, 0, 26, 18, 30, 0),
-    end: new Date(2021, 0, 26, 19, 30, 0),
-    url: "https://www.facebook.com/events/373080633839518",
-    info: "Click to learn more about our upcoming professor coffee talk!",
-    pic: "Events/Fall2020/classes_preview.png",
-    alt: "UCSB professor coffee talk"
-  },
-  {
-    id: 9,
     title: "Project Series: Connecting to a database",
     start: new Date(2021, 1, 11, 19, 0, 0),
     end: new Date(2021, 1, 11, 20, 30, 0),
@@ -105,7 +95,7 @@ export const events = [
     alt: "Connecting to a database project series graphic"
   },
   {
-    id: 10,
+    id: 9,
     title: "Project Series: Connecting to an API",
     start: new Date(2021, 1, 18, 19, 0, 0),
     end: new Date(2021, 1, 18, 20, 30, 0),
@@ -115,7 +105,7 @@ export const events = [
     alt: "Conecting to an API project series graphic"
   },
   {
-    id: 11,
+    id: 10,
     title: "Specialized Internships Panel",
     start: new Date(2021, 1, 16, 18, 30, 0),
     end: new Date(2021, 1, 16, 19, 30, 0),
@@ -125,7 +115,7 @@ export const events = [
     alt: "Specialized internship panel"
   },
   {
-    id: 12,
+    id: 11,
     title: "Project Series: Work Week",
     start: new Date(2021, 1, 25, 18, 30, 0),
     end: new Date(2021, 1, 25, 19, 30, 0),
@@ -136,7 +126,7 @@ export const events = [
     alt: "Project series work week graphic"
   },
   {
-    id: 13,
+    id: 12,
     title: "Project Series Final Showcase",
     start: new Date(2021, 2, 4, 18, 30, 0),
     end: new Date(2021, 2, 4, 19, 30, 0),
