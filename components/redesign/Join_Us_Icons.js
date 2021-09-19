18feea324a3e531f7d35476ba0029d09fc38070a
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-09-19 14:04:22 -0700

    gm - add links to join us icons

--- components/redesign/Join_Us_Icons.js
+++ components/redesign/Join_Us_Icons.js
@@ -19,15 +19,18 @@ export default function Join_Us_Icons(props) {
   const FACEBOOK_SVG = "redesign/social_icons/facebook.svg";
   const DISCORD_SVG = "redesign/social_icons/discord.svg";
   const EMAIL_SVG = "redesign/social_icons/email.svg";
-
-  const LINKTREE = "https://linktr.ee/codersSB"; /* temporary */
+  const INSTAGRAM = "https://www.instagram.com/coders.sb/?hl=en";
+  const FACEBOOK = "https://www.facebook.com/coderssb/";
+  const EMAIL =
+    "https://mail.google.com/mail/?view=cm&source=mailto&to=coderssb@gmail.com";
+  const DISCORD = "https://discord.com/invite/bW69w4QsS6";
 
   return (
     <div style={cardStyle}>
-      <Card url={LINKTREE} pic={INSTAGRAM_SVG} />
-      <Card url={LINKTREE} pic={FACEBOOK_SVG} />
-      <Card url={LINKTREE} pic={DISCORD_SVG} />
-      <Card url={LINKTREE} pic={EMAIL_SVG} />
+      <Card url={INSTAGRAM} pic={INSTAGRAM_SVG} />
+      <Card url={FACEBOOK} pic={FACEBOOK_SVG} />
+      <Card url={DISCORD} pic={DISCORD_SVG} />
+      <Card url={EMAIL} pic={EMAIL_SVG} />
     </div>
   );
 }
