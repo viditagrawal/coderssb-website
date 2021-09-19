ea386a89e5b1e8a922a70de6cc3c22b7cfabd4e2
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-09-19 13:57:48 -0700

    gm - cursor change to clickable state on hover for cards

--- components/redesign/Card.js
+++ components/redesign/Card.js
@@ -1,17 +1,30 @@
 import "../../redesign/index.css";
+import React, { useState } from "react";
 
 export default function Card(props) {
   const UPCOMING_EVENTS = "upcoming_events";
 
+  const [shouldMagnify, setShouldMagnify] = useState(false);
+
+  const style = shouldMagnify ? { cursor: "pointer" } : {};
+
   return (
-    <figure
-      onClick={() => {
-        open(props.url);
-      }}
-    >
-      <img src={props.pic} />
-      <h3 className={UPCOMING_EVENTS}>{props.title}</h3>
-      <p className={UPCOMING_EVENTS}>{props.info}</p>
-    </figure>
+    <div style={style}>
+      <figure
+        onClick={() => {
+          open(props.url);
+        }}
+        onMouseEnter={() => {
+          setShouldMagnify(true);
+        }}
+        onMouseLeave={() => {
+          setShouldMagnify(false);
+        }}
+      >
+        <img src={props.pic} />
+        <h3 className={UPCOMING_EVENTS}>{props.title}</h3>
+        <p className={UPCOMING_EVENTS}>{props.info}</p>
+      </figure>
+    </div>
   );
 }
